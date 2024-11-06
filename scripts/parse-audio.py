fefd431410b7c02909e9ec501df7987737434cd5
Nahlee Naria Khan <95993773+renee-k@users.noreply.github.com>
2024-11-06 00:57:20 -0500

    updated parse audio script

--- scripts/parse-audio.py
+++ scripts/parse-audio.py
@@ -1,14 +1,34 @@
+import os
 import whisper
+import json
 
 # Load the Whisper model
 model = whisper.load_model("medium")
 
-def transcribe_audio(audio_path):
-    # Transcribe an audio file
-    result = model.transcribe("data/annotation_1.wav") 
+def transcribe_audio(audio_path, destination_dir, filename):
+    # Transcribe the audio file
+    result = model.transcribe(audio_path) 
 
-    # Output the transcription (going to save this in a file instead)
-    print(result["text"])
+    # save transcription in a file
+    transcription_filename = f'{filename}.txt'
+    data_piece_path = os.path.join(destination_dir, transcription_filename)
 
-def create_data(annotation, transcription, question, answer):
-    return {'context': f'Annotated Session: {annotation} Explanation: {transcription}', 'question': question, 'answer': answer}
+    with open(data_piece_path, "w") as f:
+        f.write(result["text"])
+
+def create_data(annotation_file, transcription_file, question, answer):
+    # get the contents of the annotation and transcription file
+    with open(annotation_file, "r") as f:
+        annotation = f.read()
+    
+    with open(transcription_file, "r") as f:
+        transcription = f.read()
+
+    return json.dumps({
+        'context': f'Annotated Session: "{annotation}" Explanation: "{transcription}"',
+        'question': question,
+        'answer': answer
+    }, indent=4)
+
+if __name__ == "__main__":
+    transcribe_audio("data/annotation_1.wav", "data", "annotation_1")
