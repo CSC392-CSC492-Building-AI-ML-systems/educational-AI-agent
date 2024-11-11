ec09558f11fff0b1d00812edccf1846609da1f4c
Nahlee Naria Khan <95993773+renee-k@users.noreply.github.com>
2024-11-11 14:47:42 -0500

    added unit test cases for abdullah's parser

--- scripts/parse_data_abdullah_test.py
+++ scripts/parse_data_abdullah_test.py
@@ -2,15 +2,124 @@ from parse_data_abdullah import *
 import pytest
 
 def test_output_tag():
+    """
+    Test the output XML tag
+    """
     final = []
 
     filename = "tests/input_files/output_tag_test.annotated"
-    finals = parse_and_save_file(filename, "tests/output_files")
+    finals = parse_and_save_file(filename, "tests/output_files/output_test")
     final.extend(finals)
 
-    print(final)
+    assert final[-1]['User'] == "<data-piece>" + \
+                                    "<history></history>" + \
+                                    "<current-entry>" + \
+                                        "<entry>" + \
+                                            "<input></input>" + \
+                                            "<output>Linux boxtop 6. 6. 13-amd64 1 SMP PREEMPT_DYNAMIC Debian 6. 6. 13-1 (2024-01-20) x86_64Plan your installation, and FAI installs your plan. Last login Sun Sep 22 122417 2024</output>" + \
+                                            "<annotation>[BLANK]</annotation>" + \
+                                        "</entry>" + \
+                                    "</current-entry>" + \
+                                "</data-piece>"
+    
+def test_input_tag():
+    """
+    Test the input XML tag.
+    """
+    final = []
 
-    assert final[0]['User'] == """<data-piece><history></history><current-entry><entry><input></input><output>Linux boxtop 6. 6. 13-amd64 1 SMP PREEMPT_DYNAMIC Debian 6. 6. 13-1 (2024-01-20) x86_64Plan your installation, and FAI installs your plan. Last login Sun Sep 22 122417 2024</output><annotation>[BLANK]</annotation></entry></current-entry></data-piece>"""
+    filename = "tests/input_files/input_tag_test.annotated"
+    finals = parse_and_save_file(filename, "tests/output_files/input_test")
+    final.extend(finals)
+
+    assert final[-1]['User'] == "<data-piece>" + \
+                                    "<history></history>" + \
+                                    "<current-entry>" + \
+                                        "<entry>" + \
+                                            "<input>ssh 10. 0. 7. 138</input>" + \
+                                            "<output>ssh 10. 0. 7. 138</output>" + \
+                                            "<annotation>[BLANK]</annotation>" + \
+                                        "</entry>" + \
+                                    "</current-entry>" + \
+                                "</data-piece>"
+    
+def test_prompt():
+    """
+    Test the prompt.
+    """
+    final = []
+
+    filename = "tests/input_files/prompt_test.annotated"
+    finals = parse_and_save_file(filename, "tests/output_files/input_test")
+    final.extend(finals)
+
+    assert final[-1]['User'] == "<data-piece>" + \
+                                    "<history></history>" + \
+                                    "<current-entry>" + \
+                                        "<entry>" + \
+                                            "<input>ssh 10. 0. 7. 138</input>" + \
+                                            "<output>ssh 10. 0. 7. 138</output>" + \
+                                            "<annotation>[BLANK]</annotation>" + \
+                                        "</entry>" + \
+                                    "</current-entry>" + \
+                                "</data-piece>"
+    
+    assert final[-1]['Prompt'] == 'ssh into server at 10.0.7.138'
+
+def test_annotation_depth_tags():
+    """
+    Test the annotation tags.
+    """
+    final = []
+
+    filename = "tests/input_files/annotation_tag_test.annotated"
+    finals = parse_and_save_file(filename, "tests/output_files/input_test")
+    final.extend(finals)
+
+    assert final[0]['User'] == "<data-piece>" + \
+                                    "<history></history>" + \
+                                    "<current-entry>" + \
+                                        "<entry>" + \
+                                            "<input>ssh bandit12bandit. labs. overthewire. org -p 2220</input>" + \
+                                            "<output>0renee_kRenee renee_kRenee</output>" + \
+                                            "<annotation>[BLANK]</annotation>" + \
+                                            "<annotation>Subgoal Type in the ssh password for user bandit12 correctly</annotation>" + \
+                                            "<annotation>successResult</annotation>" + \
+                                            "<annotation>Tools The ssh tool from OpenSSH</annotation>" + \
+                                        "</entry>" + \
+                                    "</current-entry>" + \
+                                "</data-piece>"
+
+    assert final[0]['Prompt'] == 'Goal: ssh into bandit12@bandit.labs.overthewire.org on port 2220'
+
+def test_history_tag():
+    """
+    Test the history tag.
+    """
+    final = []
+
+    filename = "tests/input_files/history_tag_test.annotated"
+    finals = parse_and_save_file(filename, "tests/output_files/input_test")
+    final.extend(finals)
+
+    assert final[-1]['User'] == "<data-piece>" + \
+                                    "<history>" + \
+                                        "<entry>" + \
+                                            "<input></input>" + \
+                                            "<output>0demoboxtop demoboxtop</output>" + \
+                                            "<annotation></annotation>" + \
+                                        "</entry>" + \
+                                    "</history>" + \
+                                    "<current-entry>" + \
+                                        "<entry>" + \
+                                            "<input>ssh 10. 0. 7. 138</input>" + \
+                                            "<output>ssh 10. 0. 7. 138</output>" + \
+                                            "<annotation>[BLANK]</annotation>" + \
+                                        "</entry>" + \
+                                    "</current-entry>" + \
+                                "</data-piece>"
+
+    assert final[-1]['Prompt'] == 'ssh into server at 10.0.7.138'
 
 if __name__ == '__main__':
     pytest.main(['scripts/parse_data_abdullah_test.py'])
