b5d74d993535d3c1a5b27afc5a0c3770f90f056b
Abdullah Enaya <abdullah.enaya@mail.utoronto.ca>
2024-11-11 14:18:07 -0500

    no bitsandbuytes model

--- models/model4.py
+++ models/model4.py
@@ -8,18 +8,17 @@ from peft import (
     LoraConfig,
     PeftConfig,
     PeftModel,
-    get_peft_model,
-    prepare_model_for_kbit_training
+#   get_peft_model,
+#   prepare_model_for_kbit_training
 )
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
 )
 from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
-from trl.trainer.utils import add_eos_token_if_needed
-from unsloth import FastLanguageModel
+# from unsloth import FastLanguageModel
 
-max_seq_length = 2048  # Supports automatic RoPE Scaling, so choose any number
+max_seq_length = 4096  # Supports automatic RoPE Scaling, so choose any number
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 MODEL_NAME = "01-ai/Yi-6B"
 
@@ -27,16 +26,14 @@ MODEL_NAME = "01-ai/Yi-6B"
 def train_model(data):
 
     # Load model
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name=MODEL_NAME,
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME,
         device_map="auto",
         trust_remote_code=True,
-        max_seq_length=max_seq_length,
-        dtype=None,  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-        load_in_4bit=True,  # Use 4bit quantization to reduce memory usage. Can be False
-        # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+        torch_dtype=torch.float16
     )
 
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
     assert tokenizer.pad_token != tokenizer.eos_token
 
     special_tokens = {
@@ -47,25 +44,16 @@ def train_model(data):
     tokenizer.add_special_tokens(special_tokens)
     model.resize_token_embeddings(len(tokenizer))
 
-    # Do model patching and add fast LoRA weights
-    model = FastLanguageModel.get_peft_model(
-        model,
+    config = LoraConfig(
         r=16,
-        target_modules=[
-            "q_proj",
-            "k_proj",
-            "v_proj",
-            "o_proj",
-            "gate_proj",
-            "up_proj",
-            "down_proj",
-        ],
-        lora_alpha=16,
-        lora_dropout=0,  # Dropout = 0 is currently optimized
-        bias="none",  # Bias = "none" is currently optimized
-        use_gradient_checkpointing=True,
-        random_state=3407,
+        lora_alpha=32,
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM"
     )
+
+    # Do model patching and add fast LoRA weights
 
     def print_trainable_parameters(model):
         """
@@ -124,7 +112,8 @@ def train_model(data):
         model=model,
         args=training_args,
         train_dataset=dataset,
-        data_collator=collator
+        data_collator=collator,
+        peft_config=config
     )
     trainer.train()
 
