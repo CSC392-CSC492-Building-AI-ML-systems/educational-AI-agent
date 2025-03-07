fb2efb82ca4f7ba0748a263ffc5c2fac612a4a24
Brian Zhang <bria.zhang@mail.utoronto.ca>
2025-03-07 17:57:16 -0500

    Update parser1 to include event depth and command line input

--- models/Model 1/scripts/parser1.py
+++ models/Model 1/scripts/parser1.py
@@ -1,5 +1,11 @@
-xml = "1727009412.xml"
-txt = "1727009412.xml.txt"
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python parser1.py <xml> <txt>")
+    sys.exit(1)
+
+xml = sys.argv[1]
+txt = sys.argv[2]
 
 with open(xml, "r") as f:
     xml_data = f.readlines()
@@ -12,16 +18,14 @@ breaks = set(counter[i-1] for i in range(len(counter)) if counter[i] == 0)
 xml_parsed = []
 for i, line in enumerate(xml_data):
     if i in breaks:
-        xml_parsed.append("<annotation>\n")
-        xml_parsed.append("</annotation>\n")
-        xml_parsed.append("</event>\n")
-        xml_parsed.append("<event>\n")
+        xml_parsed.append("<annotation>\n</annotation>\n</event>\n<event depth=\"-1\">\n")
     elif i == 2:
-        xml_parsed.append("<event>\n")
+        xml_parsed.append("<event depth=\"-1\">\n")
     elif i == len(xml_data) - 1:
-        xml_parsed.append("</event>\n")
+        xml_parsed.append("<annotation>\n</annotation>\n</event>\n")
     xml_parsed.append(line)
 
-with open("parsed.xml", "w") as f:
+with open(f"{xml[:-4]}_parsed.xml", "w") as f:
     f.writelines(xml_parsed)
 
+print(f"Parsed XML saved as {xml[:-4]}_parsed.xml")
