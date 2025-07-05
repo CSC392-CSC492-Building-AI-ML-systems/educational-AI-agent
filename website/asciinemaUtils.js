581138d3263e75c8240961ea0c77b3b60f2244d4
Muneeb Rehman <muneeb.rehman@mail.utoronto.ca>
2025-07-05 12:24:11 -0400

    asciinemaUtils.js file created to handle asciinema conversion on website

---
+++ website/asciinemaUtils.js
@@ -0,0 +1,3 @@
+// asciinemaUtils.js serves as a utility module for handling functions relating to asciinema recordings.
+// It provides functions to generate asciinema-type recording files from the website's terminal to be used by the models.
+
