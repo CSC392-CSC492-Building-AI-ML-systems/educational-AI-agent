b728296a5d0c3d0172cd4fc441109f320d50165f
Kush Patel <kushsuren.patel@mail.utoronto.ca>
2025-08-05 16:25:09 -0400

    added json_escaper program

--- website/runpod_handler/handler.py
+++ website/runpod_handler/handler.py
@@ -219,6 +219,7 @@ def handler(job):
             "temperature": temperature,
             "do_sample": do_sample,
             "pad_token_id": tokenizer.eos_token_id,
+            # new params
             "repetition_penalty": 1.1,  # Reduce repetition
             "top_p": 0.9,  # Nucleus sampling
             "top_k": 50,   # Top-k sampling
